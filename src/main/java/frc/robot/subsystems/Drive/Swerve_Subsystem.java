1512c581857984d5739c4bde19adf2bdb1b863d1
Hunter <rothmanhunterd@gmail.com>
2023-11-14 13:50:11 -0500

    Formatting Pigeon and Swerve Odometry

--- src/main/java/frc/robot/subsystems/Drive/Swerve_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Drive/Swerve_Subsystem.java
@@ -67,8 +67,6 @@ public class Swerve_Subsystem extends SubsystemBase {
 
   private ChassisSpeeds Swerve_Speeds = new ChassisSpeeds(); // Declare Chassis Speed for use in methods
 
-  private final Pigeon2 Pigeon = new Pigeon2(Drive_Constants.Pigeon_ID, "canivore"); // Declare IMU
-
   //  Declare Swerve Module Positions for SWerve Odometry
   private SwerveModulePosition Front_Left_Position = new SwerveModulePosition(Front_Left_Swerve.Get_Drive_Position(), Front_Left_Swerve.Get_Swerve_State().angle);
   private SwerveModulePosition Front_Right_Position = new SwerveModulePosition(Front_Right_Swerve.Get_Drive_Position(), Front_Right_Swerve.Get_Swerve_State().angle);
@@ -77,11 +75,13 @@ public class Swerve_Subsystem extends SubsystemBase {
 
   private SwerveModulePosition[] Swerve_Positions = {Front_Left_Position, Front_Right_Position, Rear_Right_Position, Rear_Left_Position};
 
+  // Declare Swerve Odometry
+  private final SwerveDriveOdometry Swerve_Odometry = new SwerveDriveOdometry(Swerve, Rotation2d.fromRadians(Get_Yaw()), Swerve_Positions);
+
   // private final Pose2d Initial_Pose = new Pose2d(1.0, 1.0, Rotation2d.fromRadians(0));
   private final Pose2d Empty_Pose = new Pose2d();
 
-  // Declare Swerve Odometry
-  private final SwerveDriveOdometry Swerve_Odometry = new SwerveDriveOdometry(Swerve, Rotation2d.fromRadians(Get_Yaw()), Swerve_Positions);
+  private final Pigeon2 Pigeon = new Pigeon2(Drive_Constants.Pigeon_ID, "canivore"); // Declare IMU
 
   public Swerve_Subsystem() {
     Pigeon.configFactoryDefault();
