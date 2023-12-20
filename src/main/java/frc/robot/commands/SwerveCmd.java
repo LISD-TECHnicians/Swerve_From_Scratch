3cdc39ba67e4b08243015a4fa3441fcc2ff9d032
Hunter <rothmanhunterd@gmail.com>
2023-12-20 07:51:12 -0500

    Set Rotation Position Control Initial Heading

--- src/main/java/frc/robot/commands/SwerveCmd.java
+++ src/main/java/frc/robot/commands/SwerveCmd.java
@@ -27,6 +27,8 @@ public class SwerveCmd extends CommandBase {
 
   private ChassisSpeeds swerveSpeeds = new ChassisSpeeds(); 
 
+  private double rotationInitial; // Should make current direction "home" position for rotation position control, needs tested
+
   public SwerveCmd(SwerveSubsystem swerveSubsystem, DoubleSupplier xController, DoubleSupplier yController, DoubleSupplier rotationController, BooleanSupplier robotOriented, BooleanSupplier rotationPositionControl) {
     this.swerveSubsystem = swerveSubsystem;
     this.xController = xController;
@@ -59,10 +61,12 @@ public class SwerveCmd extends CommandBase {
     }
     else {
       if (rotationPositionControl.getAsBoolean()) {
+        for (int i = 0; i < 1; i++) {rotationInitial = swerveSubsystem.getYaw();}
+
         swerveSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
           xSpeed, 
           ySpeed, 
-          MathUtil.clamp(rotationPositionPID.calculate(swerveSubsystem.getYaw(), rotationPosition), -DriveConstants.maxSetRotationSpeed, DriveConstants.maxSetRotationSpeed), 
+          MathUtil.clamp(rotationPositionPID.calculate(swerveSubsystem.getYaw(), rotationPosition + rotationInitial), -DriveConstants.maxSetRotationSpeed, DriveConstants.maxSetRotationSpeed), 
           Rotation2d.fromRadians(swerveSubsystem.getYaw()));
       }
       else {
