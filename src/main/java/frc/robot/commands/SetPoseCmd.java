636f859712e80f343a15adbe973ea811915be81d
Hunter <rothmanhunterd@gmail.com>
2023-12-27 11:05:43 -0500

    Reformat SetPoseCmd

--- src/main/java/frc/robot/commands/SetPoseCmd.java
+++ src/main/java/frc/robot/commands/SetPoseCmd.java
@@ -9,7 +9,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 public class SetPoseCmd extends CommandBase {
   private final SwerveSubsystem swerveSubsystem;
 
-  private Pose2d pose = new Pose2d();
+  private Pose2d pose;
 
   public SetPoseCmd(SwerveSubsystem swerveSubsystem, Pose2d pose) {
     this.swerveSubsystem = swerveSubsystem;
