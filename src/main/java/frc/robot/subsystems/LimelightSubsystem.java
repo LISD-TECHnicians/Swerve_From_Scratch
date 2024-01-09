c3b5588e57b0beff0995fc57e1e7105738759035
Hunter Rothman <rothmanhunterd@gmail.com>
2024-01-09 00:17:59 -0500

    Update Limelight Test Code

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -2,43 +2,46 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import frc.robot.RobotContainer;
-import frc.robot.LimelightHelpers;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class LimelightSubsystem extends SubsystemBase {
-  private final String LL1 = "LL1";
+  private Alliance alliance;
 
-  private final GenericEntry txEntryLL1 = RobotContainer.robotStatus.add("TX", 0.0).getEntry();
-  private final GenericEntry tyEntryLL1 = RobotContainer.robotStatus.add("TY", 0.0).getEntry();
-  private final GenericEntry taEntryLL1 = RobotContainer.robotStatus.add("TA", 0.0).getEntry();
+  private NetworkTable lloneTable = NetworkTableInstance.getDefault().getTable("llone");
 
-  public LimelightSubsystem() {}
-
-  public double getTx(String limelightName) {
-    return LimelightHelpers.getTX(limelightName);
+  public LimelightSubsystem() {
+    alliance = DriverStation.getAlliance();
   }
 
-  public double getTy(String limelightName) {
-    return LimelightHelpers.getTY(limelightName);
+  public double getTx(NetworkTable networkTable) {
+    return networkTable.getEntry("tx").getDouble(0);
   }
 
-  public double getTa(String limelightName) {
-    return LimelightHelpers.getTA(limelightName);
+  public double getTy(NetworkTable networkTable) {
+    return networkTable.getEntry("ty").getDouble(0);
   }
 
-  public Pose2d getPose(String limelightName) {
-    return LimelightHelpers.getBotPose2d(limelightName);
+  public double getTa(NetworkTable networkTable) {
+    return networkTable.getEntry("ta").getDouble(0);
+  }
+
+  public double[] getPose(NetworkTable networkTable) {
+    if (alliance == Alliance.Blue) {
+      return networkTable.getEntry("botpose_wpiblue").getDoubleArray(new double[7]);
+    }
+    else {
+      return networkTable.getEntry("botpose_wpired").getDoubleArray(new double[7]);
+    }
+    
   }
 
   @Override
   public void periodic() {
-    txEntryLL1.setDouble(getTx(LL1));
-    tyEntryLL1.setDouble(getTy(LL1));
-    taEntryLL1.setDouble(getTa(LL1));
-
+    System.out.println(getTx(lloneTable));
   }
 
   @Override
