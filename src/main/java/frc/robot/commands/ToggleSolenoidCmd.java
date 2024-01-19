acef24f0e81cb495d0ee917b41fc3659f3863156
Hunter <rothmanhunterd@gmail.com>
2024-01-19 09:26:20 -0500

    Add Timer to ToggleSolenoidCmd to Test CmdGrps

--- src/main/java/frc/robot/commands/ToggleSolenoidCmd.java
+++ src/main/java/frc/robot/commands/ToggleSolenoidCmd.java
@@ -4,8 +4,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.subsystems.PneumaticsSubsystem;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class ToggleSolenoidCmd extends Command {
   private final PneumaticsSubsystem pneumaticsSubsystem;
+
+  private final Timer delayTimer = new Timer();
 
   public ToggleSolenoidCmd(PneumaticsSubsystem pneumaticsSubsystem) {
     this.pneumaticsSubsystem = pneumaticsSubsystem;
@@ -15,17 +19,24 @@ public class ToggleSolenoidCmd extends Command {
 
   @Override
   public void initialize() {
-    pneumaticsSubsystem.toggleSolenoid();
+    delayTimer.reset(); 
+    delayTimer.start();
   }
 
   @Override
-  public void execute() {}
+  public void execute() {
+    if (3 > delayTimer.get() && delayTimer.get() > 2) {
+      pneumaticsSubsystem.toggleSolenoid();
+    }
+  }
 
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    delayTimer.reset();
+  }
 
   @Override
   public boolean isFinished() {
-    return true;
+    return delayTimer.get() > 8;
   }
 }
